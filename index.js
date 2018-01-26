80261293d4c383347ffbc0925a95f5b57e6da81d
Valentina <valentina.rmo@gmail.com>
2018-01-26 17:09:44 +0000

    Try to integrate server.js with the rest of the code- This code needs to be reviewed

--- index.js
+++ index.js
@@ -1,48 +1,74 @@
 web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-abi = JSON.parse('[{"constant":false,"inputs":[{"name":"candidate", "type":"bytes32"}], "name":"totalVotesFor","outputs":[{"name":"","type":"uint8"}], "payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}], "name":"validCandidate","outputs":[{"name":"", "type":"bool"}], "payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"votesReceived","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"x","type":"bytes32"}], "name":"bytes32ToString", "outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"", "type":"uint256"}],"name":"candidateList","outputs":[{"name":"","type":"bytes32"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"voteForCandidate","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"contractOwner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"inputs":[{"name":"candidateNames","type":"bytes32[]"}],"payable":false,"type":"constructor"}]')
-VotingContract = web3.eth.contract(abi);
-// In your nodejs console, execute contractInstance.address to get the address at which the contract is deployed and change the line below to use your deployed address
-contractInstance = VotingContract.at('0x9cd95204ab37ff237d5bb701ca7be81653162229');
+asciiToHex = Web3.utils.asciiToHex
+contractInstance = new web3.eth.Contract(ABI_DEFINITION, CONTRACT_ADDRESS);
+// VotingContract = web3.eth.contract(ABI_DEFINITION)
+// contractInstance = VotingContract.at(CONTRACT_ADDRESS)
+console.log(contractInstance)
+
+accounts = web3.eth.getAccounts()
+
+
+// var accounts = accountsObj._rejectionHandler0
+console.log(accounts)
+
+// web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+// abi = JSON.parse('[{"constant":false,"inputs":[{"name":"candidate", "type":"bytes32"}], "name":"totalVotesFor","outputs":[{"name":"","type":"uint8"}], "payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}], "name":"validCandidate","outputs":[{"name":"", "type":"bool"}], "payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"votesReceived","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"x","type":"bytes32"}], "name":"bytes32ToString", "outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"", "type":"uint256"}],"name":"candidateList","outputs":[{"name":"","type":"bytes32"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"voteForCandidate","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"contractOwner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"inputs":[{"name":"candidateNames","type":"bytes32[]"}],"payable":false,"type":"constructor"}]')
+// VotingContract = web3.eth.contract(abi);
+// // In your nodejs console, execute contractInstance.address to get the address at which the contract is deployed and change the line below to use your deployed address
+// contractInstance = VotingContract.at('0x9cd95204ab37ff237d5bb701ca7be81653162229');
+
+
 candidates = {"Rama": "candidate-1", "Nick": "candidate-2", "Jose": "candidate-3"}
+validNumber = null
 
 function voteForCandidate() {
   if (validNumber == null) {
     console.log("validNumber is null")
     return
   }
+
   candidateName = $("#candidate").val();
-  contractInstance.voteForCandidate(candidateName, {from: validNumber}, function() {
-    let div_id = candidates[candidateName];
-    $("#" + div_id).html(contractInstance.totalVotesFor.call(candidateName).toString());
-    alert("Your vote has been submitted")
-  });
+
+  accounts
+  .then((accounts) => {
+    contractInstance.methods.voteForCandidate((asciiToHex(candidateName)).send({from: validNumber}), function() {
+      let div_id = candidates[candidateName];
+      $("#" + div_id).html(contractInstance.methods.totalVotesFor(asciiToHex(candidateName)).call());
+      alert("Your vote has been submitted")
+    });
+  })
 }
 
 $(document).ready(function() {
   candidateNames = Object.keys(candidates);
   for (var i = 0; i < candidateNames.length; i++) {
     let name = candidateNames[i];
-    let val = contractInstance.totalVotesFor.call(name).toString()
+    let val = contractInstance.methods.totalVotesFor(asciiToHex(name)).call()
     $("#" + candidates[name]).html(val);
   }
 });
 
-// Registers a voter as they load a page
-window.validate = function() {
-  let voterNumber = document.getElementById("account-number").value
+$("#account-number-submit").click(function() {
+  enteredNumber = document.getElementById("account-number").value
+  enterAccountNumber(enteredNumber);
 
-  try {
-    Voting.deployed().then(function(contractInstance) {
-      for(var i=0; i < web3.eth.accounts.length; i++) {
-        if (web3.eth.accounts[i] == voterNumber) {
-          validNumber = web3.eth.accounts[i]
-          contractInstance.registerVoter(web3.eth.accounts[i], { from: web3.eth.accounts[i] }).then(function() {
-            return validNumber;
-          });
-        };
-      };
-    });
-  } catch (err) {
-    console.log(err);
-  }
+})
+
+function enterAccountNumber(enteredNumber) {
+console.log(accounts)
+accounts
+.then((accounts) => {
+      for(var i=0; i < accounts.length; i++) {
+        if (accounts[i] == enteredNumber) {
+          validNumber = accounts[i];
+          console.log(i)
+          console.log("validNumber = " + validNumber)
+          console.log("contractInstance = " + contractInstance)
+          contractInstance.methods.registerVoter(asciiToHex(validNumber), {from: validNumber});
+        }
+      }
+      if (validNumber == null) {
+        alert("That's not a valid account number");
+      }
+      })
 }
---
+++ server.js
@@ -0,0 +1,77 @@
+#!/usr/bin/env node
+
+
+const Web3 = require('web3');
+const solc = require('solc');
+const fs = require('fs');
+const http = require('http');
+
+const provider = new Web3.providers.HttpProvider("http://localhost:8545")
+const web3 = new Web3(provider);
+const asciiToHex = Web3.utils.asciiToHex;
+
+const candidates = ['Rama', 'Nick', 'Jose'];
+
+web3.eth.getAccounts()
+.then((accounts) => {
+  const code = fs.readFileSync('./contracts/Voting.sol').toString();
+  const compiledCode = solc.compile(code);
+  const errors = [];
+  const warnings = [];
+  (compiledCode.errors || []).forEach((err) => {
+    if (/\:\s*Warning\:/.test(err)) {
+      warnings.push(err);
+    } else {
+      errors.push(err);
+    }
+  });
+
+  if (errors.length) {
+    throw new Error('solc.compile: ' + errors.join('\n'));
+  }
+  if (warnings.length) {
+  }
+  const byteCode = compiledCode.contracts[':Voting'].bytecode;
+  const abiDefinition = JSON.parse(compiledCode.contracts[':Voting'].interface);
+
+  const VotingContract = new web3.eth.Contract(abiDefinition,
+    {data: byteCode, from: accounts[0], gas: 4700000}
+  );
+
+  let deployedContract = null;
+
+  VotingContract.deploy({arguments: [candidates.map(asciiToHex)]})
+  .send(function (error, transactionHash) {
+  })
+  .then((result) => {
+    deployedContract = result;
+    deployedContract.setProvider(provider);
+
+  })
+  .then(() => {
+    const server = http.createServer((req, res) => {
+      res.writeHead(200);
+      let fileContents = '';
+      try {
+        fileContents = fs.readFileSync(__dirname + req.url, 'utf8');
+      } catch (e) {
+        fileContents = fs.readFileSync(__dirname + '/index.html', 'utf8');
+      }
+      res.end(
+        fileContents.replace(
+          /REPLACE_WITH_CONTRACT_ADDRESS/g,
+          deployedContract.options.address
+        ).replace(
+          /REPLACE_WITH_ABI_DEFINITION/g,
+          compiledCode.contracts[':Voting'].interface
+        )
+      );
+    });
+    server.on('clientError', (err, socket) => {
+      socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
+    });
+    server.listen(8000, () => {
+      console.log('Listening on localhost:8000');
+    });
+  });
+});
