8af22da6b5c2a5b02c860fc80dc437e95d49e886
Eleanor Wintram <eleanorwintram@gmail.com>
2018-01-26 12:36:19 +0000

    updates jquery to make table visible only when registration is successful

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 abi = JSON.parse('[{"constant":true,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"totalVotesFor","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"account","type":"address"}],"name":"registerVoter","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"validCandidate","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"votesReceived","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"voters","outputs":[{"name":"voted","type":"bool"},{"name":"registered","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"candidateList","outputs":[{"name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"voteForCandidate","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"candidateNames","type":"bytes32[]"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"voter","type":"address"}],"name":"Registered","type":"event"}]')
 VotingContract = web3.eth.contract(abi);
 // In your nodejs console, execute contractInstance.address to get the address at which the contract is deployed and change the line below to use your deployed address
-contractInstance = VotingContract.at('0xd3bcb506ae613205889af5b39c7f5c736b32f66f');
+contractInstance = VotingContract.at('0xba24c66dc730e01c00b73a61ef87187bff68d94b');
 candidates = {"Rama": "candidate-1", "Nick": "candidate-2", "Jose": "candidate-3"};
 validNumber = null
 
@@ -40,9 +40,7 @@ function enterAccountNumber(enteredNumber) {
         if (web3.eth.accounts[i] == enteredNumber) {
           validNumber = web3.eth.accounts[i];
           $("#vote-page").show()
-          // console.log(i)
-          // console.log("validNumber = " + validNumber)
-          // console.log("contractInstance = " + contractInstance)
+          $("#account-number").val(" ")
           contractInstance.registerVoter(validNumber, {from: validNumber});
         }
       }
